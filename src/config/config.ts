d27bad2acc7cece05dc7e1d2dffb4ace1a10fb6c
tamlv.bhsoft <tamlv.bhsoft@gmail.com>
2021-12-22 00:25:06 +0700

    fix port to env variable

--- src/config/config.ts
+++ src/config/config.ts
@@ -27,7 +27,7 @@ const MONGO = {
 };
 
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || "localhost";
-const SERVER_PORT = process.env.SERVER_PORT || 1337;
+const SERVER_PORT = process.env.SERVER_PORT || 5000;
 const SERVER_TOKEN_EXPIRETIME = process.env.SERVER_TOKEN_EXPIRETIME || 3600;
 const SERVER_TOKEN_ISSUER = process.env.SERVER_TOKEN_ISSUER || "coolIssuer";
 const SERVER_TOKEN_SECRET = process.env.SERVER_TOKEN_SECRET || "superscret";
